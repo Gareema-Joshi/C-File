1015b6dab02e759946ebaedbe2fe00d519132a82
Gareema Joshi <gareemajoshi07@gmail.com>
2026-03-27 00:06:21 +0530

    Pattern in C

---
+++ pattern.c
@@ -0,0 +1,174 @@
+#include <stdio.h>
+int main()
+{
+  for(int i=1;i<=5;i++)
+  {
+    for(int j=1;j<=5;j++)
+    {
+      if(j==1||j==5||i==3)
+      {
+        printf("+ ");
+      }
+      else
+      {
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+   printf("\n");
+  printf("\n");
+  for(int i=1;i<=5;i++)
+  {
+    for(int j=1;j<=5;j++)
+    {
+      if(j==1||i==5)
+      {
+        printf("+ ");
+      }
+      else
+      {
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+   printf("\n");
+  printf("\n");
+  for(int i=1;i<=5;i++)
+  {
+    for(int j=1;j<=5;j++)
+    {
+      if(j==3||i==1)
+      {
+        printf("+ ");
+      }
+      else
+      {
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+   printf("\n");
+  printf("\n");
+  for(int i=1;i<=5;i++)
+  {
+    for(int j=1;j<=5;j++)
+    {
+      if(j==1||i==3||i==1)
+      {
+        printf("+ ");
+      }
+      else
+      {
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+   printf("\n");
+  printf("\n");
+  for(int i=1;i<=5;i++)
+  {
+    for(int j=1;j<=5;j++)
+    {
+      if(j==3||i==5||i==1)
+      {
+        printf("+ ");
+      }
+      else
+      {
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+   printf("\n");
+  printf("\n");
+  for(int i=1;i<=5;i++)
+  {
+    for(int j=1;j<=5;j++)
+    {
+      if(j==1||i==3||i==1||i==5)
+      {
+        printf("+ ");
+      }
+      else
+      {
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+  printf("\n");
+  printf("\n");
+  for(int i=1;i<=5;i++)
+  {
+    for(int j=1;j<=5;j++)
+    {
+      if(j==1||j==5||i==j)
+      {
+        printf("& ");
+      }
+      else
+      {
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+  printf("\n");
+  printf("\n");
+  for(int i=1;i<=5;i++)
+  {
+    for(int j=1;j<=5;j++)
+    {
+      if((i+j)==6||i==5||i==1)
+      {
+        printf("$ ");
+      }
+      else
+      {
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+  printf("\n");
+  printf("\n");
+  for(int i=1;i<=5;i++)
+  {
+    for(int j=1;j<=5;j++)
+    {
+      if(i==1||i==3||i==5||j==1&&i<=3||i*j==20)
+      {
+        printf("# ");
+      }
+      else
+      {
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+  printf("\n");
+  printf("\n");
+  for(int i=1;i<=5;i++)
+  {
+    for(int j=1;j<=5;j++)
+    {
+      if(j==1||i==3||i==1||i==5)
+      {
+        printf("+ ");
+      }
+      else
+      {
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+}
+
+
